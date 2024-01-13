5c3d6d8cf8754eb66b0216341a5a220ea22a8e25
MuraliAirody <muraliairody@gmail.com>
2024-01-14 01:11:37 +0530

    mongo db configuration

--- Ecommerce_Java/src/main/java/com/ecommerce/EcommerceJavaApplication.java
+++ Ecommerce_Java/src/main/java/com/ecommerce/EcommerceJavaApplication.java
@@ -2,8 +2,10 @@ package com.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(".env")
 public class EcommerceJavaApplication {
 
 	public static void main(String[] args) {
