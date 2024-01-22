6db33dfa3cbb97fb39b74fb33c80e8871d0f3094
MuraliAirody <muraliairody@gmail.com>
2024-01-22 20:09:54 +0530

    order cart product reducers

--- Ecommerce_UI/src/redux/store.js
+++ Ecommerce_UI/src/redux/store.js
@@ -1,9 +1,16 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import {thunk} from 'redux-thunk';
 import authReducer from './Auth/reducer';
+import customerProductReducer from './customer/product/reducer';
+import cartReducer from './customer/cart/reducer';
+import { orderReducer } from './customer/order/reducer';
 
 const rootReducers = combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    customersProduct:customerProductReducer,
+    cart:cartReducer,
+    order:orderReducer,
+
 });
 
 export const store = createStore(rootReducers, applyMiddleware(thunk));
